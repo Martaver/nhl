85e35bb215136ad68ba12e569af58646a2bd6717
Sebastian Nemeth <sebastian@monkmage.com>
2018-11-24 15:36:42 +0200

    fix(ops): buildpack doesn't run webdev build

--- setup-heroku.sh
+++ setup-heroku.sh
@@ -8,11 +8,11 @@ heroku git:remote -a nhlapp
 heroku config:set DART_SDK_URL=https://storage.googleapis.com/dart-archive/channels/stable/release/latest/sdk/dartsdk-linux-x64-release.zip
 
 # Specifies the Heroku Buildpack to use
-heroku config:set BUILDPACK_URL=https://github.com/stablekernel/heroku-buildpack-dart.git
+heroku buildpacks:set https://github.com/graphicbeacon/heroku-buildpack-dart.git
 
 # Configure dart environment
 heroku config:set PATH=/app/bin:/usr/local/bin:/usr/bin:/bin:/app/.pub-cache/bin:/app/dart-sdk/bin
 heroku config:set PUB_CACHE=/app/pub-cache
 
 # Configure dart build on buildpack
-heroku config:set DART_BUILD_CMD="/app/dart-sdk/bin/pub global activate webdev && /app/dart-sdk/bin/pub global run webdev build"
+heroku config:set DART_BUILD_CMD="/app/dart-sdk/bin/pub global activate webdev && /app/dart-sdk/bin/pub global run webdev build && ls -a /app && ls -a /app/build"
