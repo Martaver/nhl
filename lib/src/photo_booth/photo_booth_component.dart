86ca39ecef0a7edadeed1292441976a70c587ac9
Sebastian Nemeth <sebastian@monkmage.com>
2018-11-25 21:05:37 +0200

    chore(review): remove old TS code

--- lib/src/photo_booth/photo_booth_component.dart
+++ lib/src/photo_booth/photo_booth_component.dart
@@ -37,11 +37,7 @@ class PhotoBoothComponent implements AfterViewInit, OnDestroy {
 
   @Output() Stream<GotSnapshotEvent> get gotSnapshot => gotSnapshotController.stream;
 
-  // State: State = State;
   State state = State.Recording;
-
-  // @ViewChild("video") ElementRef videoRef;
-  // VideoElement get video => videoRef.nativeElement;
 
   @ViewChild("video") VideoElement video;
   @ViewChild("canvas") CanvasElement canvas;
@@ -57,10 +53,7 @@ class PhotoBoothComponent implements AfterViewInit, OnDestroy {
   static MediaStream STREAM;
 
   ngAfterViewInit() {
-    
-    // video = this.element.nativeElement.querySelector('#recorder').querySelector('video');
-    // this.navigator.getUserMedia = this.navigator.getUserMedia || this.navigator.webkitGetUserMedia || this.navigator.mozGetUserMedia || this.navigator.msGetUserMedia;
-
+  
     if (STREAM != null) {
 
       releaseVideo();
@@ -71,17 +64,6 @@ class PhotoBoothComponent implements AfterViewInit, OnDestroy {
       
       STREAM = stream; // make stream available to console
       video.srcObject = stream;
-      //Success      
-      // if (window.navigator.mozGetUserMedia) {
-      //   // this.video.mozSrcObject = stream;
-      //   video.srcObject = stream;
-      // } else {
-      //   var vendorUrl = window.URL;
-      //   if(vendorUrl == null) vendorUrl = window.webkitURL;
-      //   video.src = vendorUrl.createObjectURL(stream);
-        
-      // }
-
       video.play();
 
     });    
@@ -97,17 +79,13 @@ class PhotoBoothComponent implements AfterViewInit, OnDestroy {
   snapshot() {
 
     if(STREAM != null) {
-
-      // this.canvas = this.element.nativeElement.querySelector('#recorder').querySelector('canvas');
+      
       canvas.width = video.videoWidth;
       canvas.height = video.videoHeight;
 
       context = canvas.getContext('2d');
       context.drawImage(video, 0, 0);
-
-      //For some reason, image/jpeg is much more efficient than png.
-      //Theory 1 : It could be that toDataURL is encoding a jpeg & compressing.
-      //Theory 2 : It could have been that prevously when saving as png, when we upload a jpeg it gets decompressed and saved as a png and got huge.
+      
       imgSrc = canvas.toDataUrl('image/jpeg');
 
       state = State.Confirming;
@@ -125,12 +103,6 @@ class PhotoBoothComponent implements AfterViewInit, OnDestroy {
 
     if(STREAM != null) {
 
-      //Old, deprecated way of stopping the stream using MediaStream object.
-      // if (STREAM.stop != null) { STREAM.stop(); }
-
-      //New track-based approach to managing audio & video streams.
-      // else {
-
         STREAM.getAudioTracks().forEach((track) {
           track.stop();
         });
@@ -138,9 +110,6 @@ class PhotoBoothComponent implements AfterViewInit, OnDestroy {
         STREAM.getVideoTracks().forEach((track) {
           track.stop();
         });
-      // }
-
-      // this.window.stream = null;
     }
   }
 
@@ -148,7 +117,6 @@ class PhotoBoothComponent implements AfterViewInit, OnDestroy {
 
     video.pause();
     video.src = null;
-    // if(this.video.hasOwnProperty("mozSrcObject")) this.video["mozSrcObject"] = null;
   }
 
   ngOnDestroy() {
